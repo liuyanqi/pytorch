946cbe54583ab8e1a9e689933cd3cc645912f546
Yanqi Liu <liuyanqi@umich.edu>
2017-09-28 20:55:48 -0400

    fine tune for two branches and edit heatmap

--- finetune.py
+++ finetune.py
@@ -12,18 +12,21 @@ import torchvision.transforms as transforms
 import os
 import argparse
 import time
+import pickle
+
+from custom_dataset import CustomFolder
 
 #import torchvision.models as models
 from models import *
 from utils import progress_bar
 from torch.autograd import Variable
 import torchvision.models as models
-
+from collections import defaultdict
 
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
-parser.add_argument('--epoch', default=10, type=int, help='number of epoches')
+parser.add_argument('--epoch', default=3, type=int, help='number of epoches')
 parser.add_argument('--dr', default=1, type=float, help='data reduction denominator')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 args = parser.parse_args()
@@ -34,38 +37,86 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.Scale((224,224)),
     transforms.ToTensor(),
     transforms.Normalize((0.484, 0.456, 0.406), (0.229, 0.224, 0.225)),
 ])
 
 transform_test = transforms.Compose([
-    transforms.Scale((224,224)),
     transforms.ToTensor(),
     transforms.Normalize((0.484, 0.456, 0.406), (0.229, 0.224, 0.225))
 ])
 
 
-trainset = torchvision.datasets.ImageFolder(root='./data/umich/train/', transform=transform_train)
+trainset = CustomFolder(root='./data/umich_shape/train/', transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=40, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.ImageFolder(root='./data/umich/test/', transform=transform_test)
+testset = CustomFolder(root='./data/umich_shape/test/', transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=40, shuffle=True, num_workers=2)
 
 print("==> Loading model..")
-net = models.vgg11(pretrained=True)
-mod = list(net.classifier.children())
-mod.pop()
-mod.append(nn.Linear(4096,16))
-new_classifier = nn.Sequential(*mod)
-net.classifier = new_classifier
 
-ignored_params = list(map(id,list(net.classifier.children())[-1].parameters()))
-base_params = filter(lambda p: id(p) not in ignored_params, net.parameters())
+vgg11 = models.vgg11(pretrained=True)
+vgg11_head = nn.Sequential(*list(vgg11.children())[:-1])
+# mod = list(net.classifier.children())
+# mod.pop()
+# mod.append(nn.Linear(4096,16))
+
+class Two_Branch_Net(nn.Module):
+	def __init__(self):
+		super(Two_Branch_Net, self).__init__()
+
+		self.base_model = vgg11_head
+
+		self.classifier = nn.Sequential(
+			nn.Conv2d(512, 4096, kernel_size=7),
+			nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 4096, kernel_size=1),
+			nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 16, kernel_size=1)
+			)
+
+		self.classifier_shape = nn.Sequential(
+			nn.Conv2d(512, 4096, kernel_size=7),
+			nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 4096, kernel_size=1),
+			nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 6, kernel_size=1)
+			)
+
+	def forward(self, x):
+		out = self.base_model(x)
+		# out = out.view(out.size(0), -1)
+		out1 = self.classifier(out)
+		out2 = self.classifier_shape(out)
+		out1 = out1.view(out1.size(0), out1.size(1))
+		out2 = out2.view(out2.size(0), out2.size(1))
+
+		return out1, out2
+
+net = Two_Branch_Net()
+#load the pretrained coefficient
+net.base_model.load_state_dict(vgg11_head.state_dict())
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+
 optimizer = optim.SGD([
-    	{'params': base_params},
-    	{'params': list(net.classifier.children())[-1].parameters(), 'lr':args.lr}
-    	], lr=args.lr, momentum=0.9)
+							{'params': net.base_model.parameters()},
+							{'params': net.classifier.parameters(), 'lr': 0.02},
+							{'params': net.classifier_shape.parameters()}
+						], lr=0.01, momentum=0.9)
+
+# new_classifier = nn.Sequential(*mod)
+# net.classifier = new_classifier
+
+# ignored_params = list(map(id,list(net.classifier.children())[-1].parameters()))
+# base_params = filter(lambda p: id(p) not in ignored_params, net.parameters())
+# optimizer = optim.SGD([
+#     	{'params': base_params},
+#     	{'params': list(net.classifier.children())[-1].parameters(), 'lr':args.lr}
+#     	], lr=args.lr, momentum=0.9)
 
 criterion = nn.CrossEntropyLoss()
 
@@ -73,9 +124,6 @@ if use_cuda:
     net.cuda()
     net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
     cudnn.benchmark = True
-    # optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-
-    	
     	
 
 # Training
@@ -84,31 +132,38 @@ def train():
     train_loss = 0
     correct = 0
     total = 0
+    total_shape = 0
+    correct_shape =0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         if batch_idx % args.dr != 0:
             continue 
 
         if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda()
+            inputs, targets1, targets2 = inputs.cuda(), targets[0].cuda(), targets[1].cuda()
         optimizer.zero_grad()
-        inputs, targets = Variable(inputs), Variable(targets)
+        inputs, targets1, targets2 = Variable(inputs), Variable(targets1), Variable(targets2)
 
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
+        outputs1, outputs2 = net(inputs)
+        loss1 = criterion(outputs1, targets1)
+        loss2 = criterion(outputs2, targets2)
+        torch.autograd.backward([loss1, loss2])
+        # torch.autograd.backward(loss1)
         optimizer.step()
 
-        train_loss += loss.data[0]
-        _, predicted = torch.max(outputs.data, 1)
-        # print("target:", str(targets.data.cpu()))
-        # print("output: ", str(outputs.data.cpu()))
-        # print("predicted: ", str(Variable(predicted).data.cpu()))
+        # train_loss += loss.data[0]
+        _, predicted = torch.max(outputs1.data, 1)
+        _, predicted_shape = torch.max(outputs2.data, 1)
 
-        total += targets.size(0)
-        correct += predicted.eq(targets.data).cpu().sum()
+
+        total += targets1.size(0)
+        correct += predicted.eq(targets1.data).cpu().sum()
+
+        total_shape += targets2.size(0)
+        correct_shape += predicted_shape.eq(targets2.data).cpu().sum()
+
         print("traning: ", 100*correct/total)
-        # progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-        #     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        print("traning shape: ", 100*correct_shape/total_shape)
+
 
 
 def test():
@@ -117,20 +172,28 @@ def test():
     test_loss = 0
     correct = 0
     total = 0
+    total_shape = 0
+    correct_shape =0
     for batch_idx, (inputs, targets) in enumerate(testloader):
         if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda()
-        inputs, targets = Variable(inputs, volatile=True), Variable(targets)
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
+            inputs, targets1, targets2 = inputs.cuda(), targets[0].cuda(), targets[1].cuda()
+        
+        inputs, targets1, targets2 = Variable(inputs, volatile=True), Variable(targets1), Variable(targets2)
+        outputs1, outputs2 = net(inputs)
 
-        test_loss += loss.data[0]
-        _, predicted = torch.max(outputs.data, 1)
-        total += targets.size(0)
-        correct += predicted.eq(targets.data).cpu().sum()
-        print("test: ", str(100*correct/total))
-        # progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-        #     % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+        _, predicted = torch.max(outputs1.data, 1)
+        _, predicted_shape = torch.max(outputs2.data, 1)
+        
+        total += targets1.size(0)
+        correct += predicted.eq(targets1.data).cpu().sum()
+
+        total_shape += targets2.size(0)
+        correct_shape += predicted_shape.eq(targets2.data).cpu().sum()
+        
+        print("testing: ", 100*correct/total)
+        print("testing shape: ", 100*correct_shape/total_shape)
+
    
     # Save checkpoint.
     acc = 100.*correct/total
@@ -150,3 +213,64 @@ def test():
 for i in range(args.epoch):
 	train()
 	test()
+
+
+print('Saving..')
+state = {
+    'net': net.module if use_cuda else net,
+}
+if not os.path.isdir('checkpoint'):
+    os.mkdir('checkpoint')
+torch.save(state, './checkpoint/model_vgg11.ta')
+
+# ##save weight
+# weight_dict=defaultdict(dict)
+# #64*3*3*3
+# weight_dict["conv1"]["weight"] = net.module.features[0].state_dict()["weight"].cpu().numpy()
+# weight_dict["conv1"]["bias"] = net.module.features[0].state_dict()["bias"].cpu().numpy()
+
+# #128*3*3*3
+# weight_dict["conv2"]["weight"] = net.module.features[3].state_dict()["weight"].cpu().numpy()
+# weight_dict["conv2"]["bias"] = net.module.features[3].state_dict()["bias"].cpu().numpy()
+
+# #256*3*3*3
+# weight_dict["conv3"]["weight"] = net.module.features[6].state_dict()["weight"].cpu().numpy()
+# weight_dict["conv3"]["bias"] = net.module.features[6].state_dict()["bias"].cpu().numpy()
+
+
+# #256*3*3*3
+# weight_dict["conv4"]["weight"] = net.module.features[8].state_dict()["weight"].cpu().numpy()
+# weight_dict["conv4"]["bias"] = net.module.features[8].state_dict()["bias"].cpu().numpy()
+
+# #512*3*3*3
+# weight_dict["conv5"]["weight"] = net.module.features[11].state_dict()["weight"].cpu().numpy()
+# weight_dict["conv5"]["bias"] = net.module.features[11].state_dict()["bias"].cpu().numpy()
+
+# #512*3*3*3
+# weight_dict["conv6"]["weight"] = net.module.features[13].state_dict()["weight"].cpu().numpy()
+# weight_dict["conv6"]["bias"] = net.module.features[13].state_dict()["bias"].cpu().numpy()
+
+# #512*3*3*3
+# weight_dict["conv7"]["weight"] = net.module.features[16].state_dict()["weight"].cpu().numpy()
+# weight_dict["conv7"]["bias"] = net.module.features[16].state_dict()["bias"].cpu().numpy()
+
+# #512*3*3*3
+# weight_dict["conv8"]["weight"] = net.module.features[18].state_dict()["weight"].cpu().numpy()
+# weight_dict["conv8"]["bias"] = net.module.features[18].state_dict()["bias"].cpu().numpy()
+
+# #fc layer 25088->4096
+# weight_dict["fc1"]["weight"] = net.module.classifier[0].state_dict()["weight"].cpu().numpy()
+# weight_dict["fc1"]["bias"] = net.module.classifier[0].state_dict()["bias"].cpu().numpy()
+
+# #fc layer 4096->4096
+# weight_dict["fc2"]["weight"] = net.module.classifier[3].state_dict()["weight"].cpu().numpy()
+# weight_dict["fc2"]["bias"] = net.module.classifier[3].state_dict()["bias"].cpu().numpy()
+
+# #fc layer 4096->16
+# weight_dict["fc3"]["weight"] = net.module.classifier[6].state_dict()["weight"].cpu().numpy()
+# weight_dict["fc3"]["bias"] = net.module.classifier[6].state_dict()["bias"].cpu().numpy()
+
+# # torch.save(weight_dict, './checkpoint/trained_model_vgg11.t7')
+
+# with open('./checkpoint/trained_model_vgg11.t7', 'wb') as f:
+# 	pickle.dump(weight_dict, f, pickle.HIGHEST_PROTOCOL)
--- heatmap.py
+++ heatmap.py
@@ -25,73 +25,122 @@ import matplotlib.patches as patches
 import numpy as np
 import math
 
+
+
+class Two_Branch_Net(nn.Module):
+	def __init__(self):
+		super(Two_Branch_Net, self).__init__()
+
+		self.base_model = vgg11_head
+
+		self.classifier = nn.Sequential(
+			nn.Conv2d(512, 4096, kernel_size=7),
+			nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 4096, kernel_size=1),
+			nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 16, kernel_size=1)
+			)
+
+		self.classifier_shape = nn.Sequential(
+			nn.Conv2d(512, 4096, kernel_size=7),
+			nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 4096, kernel_size=1),
+			nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 6, kernel_size=1)
+			)
+
+	def forward(self, x):
+		out = self.base_model(x)
+		# out = out.view(out.size(0), -1)
+		out1 = self.classifier(out)
+		out2 = self.classifier_shape(out)
+		print(out1)
+		out1 = out1.view(out1.size(1), out1.size(2)*out1.size(3))
+		out2 = out2.view(out2.size(1), out2.size(2)*out2.size(3))
+
+		return out1, out2
+
+
+imsize = 700
+imwidth = 640
+imheight = 480
+
 sns.set()
 use_cuda = torch.cuda.is_available()
 LABELS_URL = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
 
 print("=====>Loading model...")
-vgg11 = models.vgg11(pretrained=True)
+# vgg11 = models.vgg11(pretrained=True)
 
 convnet = [[3,1,1], [2,2,0], [3,1,1], [2,2,0], [3,1,1], [3,1,1], [2,2,0], [3,1,1], [3,1,1], [2,2,0], [3,1,1], [3,1,1], [2,2,0], [7,1,0], [1,1,0], [1,1,0]]
 layer_names = ['conv1', 'pool1', 'conv2', 'pool2', 'conv3', 'conv4', 'pool3','conv5', 'conv6', 'pool4', 'conv7', 'conv8', 'pool5', 'conv9', 'conv10', 'conv11']
-imsize = 700
 
 
-vgg11_head = nn.Sequential(*list(vgg11.children())[:-1])
-class Fully_Conv_Vgg(nn.Module):
-	def __init__(self):
-		super(Fully_Conv_Vgg, self).__init__()
+#class definition for fully convolutional vgg11
+#vgg11_head contains all the convolutional layers in vgg11
+#then fully_conv_vgg append three more convolutional layers to vgg11_head
+# vgg11_head = nn.Sequential(*list(vgg11.children())[:-1])
+# class Fully_Conv_Vgg(nn.Module):
+# 	def __init__(self):
+# 		super(Fully_Conv_Vgg, self).__init__()
 
-		self.base_model = vgg11_head
-		self.conv1 = nn.Conv2d(512,4096, kernel_size=7)
-		self.conv2 = nn.Conv2d(4096,4096, kernel_size=1)
-		self.conv3 = nn.Conv2d(4096, 1000, kernel_size=1)
+# 		self.base_model = vgg11_head
+# 		self.conv1 = nn.Conv2d(512,4096, kernel_size=7)
+# 		self.conv2 = nn.Conv2d(4096,4096, kernel_size=1)
+# 		self.conv3 = nn.Conv2d(4096, 1000, kernel_size=1)
 
-	def forward(self, x):
-		out = self.base_model(x)
-		print(out)
-		out = self.conv1(out)
-		out = self.conv2(out)
-		out = self.conv3(out)
+# 	def forward(self, x):
+# 		out = self.base_model(x)
+# 		print(out)
+# 		out = self.conv1(out)
+# 		out = self.conv2(out)
+# 		out = self.conv3(out)
 
-		return out
+# 		return out
 
-model = Fully_Conv_Vgg()
-#load the pretrained coefficient
-model.base_model.load_state_dict(vgg11_head.state_dict())
-model.conv1.load_state_dict({"weight": vgg11.classifier[0].state_dict()["weight"].view(512, 4096,7,7),
-	                         "bias":vgg11.classifier[0].state_dict()["bias"]})
-model.conv2.load_state_dict({"weight": vgg11.classifier[3].state_dict()["weight"].view(4096, 4096,1,1),
-	                         "bias":vgg11.classifier[3].state_dict()["bias"]})
-model.conv3.load_state_dict({"weight": vgg11.classifier[6].state_dict()["weight"].view(1000, 4096,1,1),
-	                         "bias":vgg11.classifier[6].state_dict()["bias"]})
+# model = Fully_Conv_Vgg()
+# #load the pretrained coefficient
+# model.base_model.load_state_dict(vgg11_head.state_dict())
+# #reshape the pre-trained fc layer coefficient into convolutional layer coeffcient form
+# model.conv1.load_state_dict({"weight": vgg11.classifier[0].state_dict()["weight"].view(512, 4096,7,7),
+# 	                         "bias":vgg11.classifier[0].state_dict()["bias"]})
+# model.conv2.load_state_dict({"weight": vgg11.classifier[3].state_dict()["weight"].view(4096, 4096,1,1),
+# 	                         "bias":vgg11.classifier[3].state_dict()["bias"]})
+# model.conv3.load_state_dict({"weight": vgg11.classifier[6].state_dict()["weight"].view(1000, 4096,1,1),
+# 	                         "bias":vgg11.classifier[6].state_dict()["bias"]})
 
-#model = vgg11
+checkpoint = torch.load('./checkpoint/model_vgg11.ta')
+model = checkpoint['net']
+
+
 
 if use_cuda:
     model.cuda()
     model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count()))
     cudnn.benchmark = True
 
+#preprocess crop the image at the center by imsize; this can be changed but notice that it will result in different output size
 preprocess = transforms.Compose([
-	#transforms.Scale(256),
-    transforms.CenterCrop(700),
     transforms.ToTensor(),
     transforms.Normalize((0.484, 0.456, 0.406), (0.229, 0.224, 0.225)),
 ])
 
+
+#preprocess2 is the same as preprocess but remove the normalization for visualization
 preprocess2 = transforms.Compose([
-	#transforms.Scale(256),
-    transforms.CenterCrop(700),
     transforms.ToTensor()
 ])
-#receptive field size - 1 + 2 + 1 + 2*2 + 2 + 2*4 +2*4+ 4+2*8 + 2*8+8 + 2*16+ 2*16+ 16 = 150
+
+#receptive field size 
 # j(out) = j(in)*s
 # r(out) = r(in) + (k-1)*j(in)
 # start(out) = start(in) + ((k-1)/2-p)*j(in)
 # maxpool layer is filter with kernal size 2 and stride 2
 # start(in) + 1*1 + 1*2 + 1*4 + 1*4 + 1*8 + 1*8 + 1*16+1*16 = 59
-
 
 
 def outFromIn(conv, layerIn):
@@ -121,51 +170,58 @@ def receptive_field(idx_x, idx_y):
   layerInfos=[]
   #first layer is the data layer (image) with n_0 = image size; j_0 = 1; r_0 = 1; and start_0 = 0.5
   print ("-------Net summary------")
-  currentLayer = [imsize, 1, 1, 0.5]
+  currentLayer = [imwidth, 1, 1, 0.5]
+  currentLayer2 = [imheight, 1, 1, 0.5]
   printLayer(currentLayer, "input image")
   for i in range(len(convnet)):
     currentLayer = outFromIn(convnet[i], currentLayer)
+    currentLayer2 = outFromIn(convnet[i], currentLayer2)
     layerInfos.append(currentLayer)
     printLayer(currentLayer, layer_names[i])
   print ("------------------------")
 
-  
   n = layerInfos[-1][0]
-  j = layerInfos[-1][0]
+  j = layerInfos[-1][1]
   r = layerInfos[-1][2]
   start = layerInfos[-1][3]
+  start_2 = currentLayer2[3]
+  j_2 = currentLayer2[1]
 
-
-  return (start+idx_x*j, start+idx_y*j, r)
+  #return the center coordinate of the receptive field with its corresponding size
+  return (start+idx_x*j, start_2+idx_y*j_2, r)
 
   
 print("=====>Loading image...")
-#image = Image.open('./data/input2.jpeg')
-image = Image.open('./data/input5.jpg')
+# image = Image.open('./data/input2.jpeg')
+image = Image.open('./data/scenergb_3.jpg')
 im2 = preprocess2(image)
 image = preprocess(image)
 image = Variable(image)
 image = image.unsqueeze(0)
 image = image.cuda()
  
-outputs = model(image)
+outputs, outputs_shape = model(image)
 
 heatmap = outputs.data.cpu().numpy()
-cat_heatmap = heatmap[:,285,:,:]
-cat_heatmap = cat_heatmap.reshape(15,15)
+# 285 corresponds to the class index of cat
+# you can change it to other class if you are not detecting cat
+cat_heatmap = heatmap[6,:]
+cat_heatmap = cat_heatmap.reshape(9,14)
 
+#pick the largest response of heatmap but you can change it to a threshold here
 (idx_x, idx_y) = np.unravel_index(cat_heatmap.argmax(), cat_heatmap.shape)
-(mid_x, mid_y, r) = receptive_field(idx_x, idx_y)
+(mid_x, mid_y, r) = receptive_field(idx_y, idx_x)
+print("idx_x: " + str(idx_x) + "idx_y: " + str(idx_y))
 print("mid_x: " + str(mid_x) + " mid_y: " + str(mid_y)+" with size: " + str(r))
 
+r=100
 
 
+fig1 = plt.figure(1)
+ax = fig1.add_subplot(211, aspect='equal')
+sns.heatmap(cat_heatmap)
 
-plt.figure(1)
-ax = sns.heatmap(cat_heatmap)
-
-fig2 = plt.figure(2)
-ax = fig2.add_subplot(111, aspect='equal')
+ax = fig1.add_subplot(212, aspect='equal')
 
 im = Variable(im2).data.cpu().numpy()
 ax.imshow(np.transpose(im,(1,2,0)))
@@ -182,10 +238,6 @@ plt.show()
 # print(labels[predicted.data.cpu().numpy()[0]])
 
 
-#convert intermidiate fc layer to conv layer
-#load pre-trained fc layer's weights to new convlutional's kernal
-# module.conv.load_state_dict({"weight": model.fc.state_dict()["weight"].view(1000, 2048,1, 1), 
-# 	                         "bias": model.fc.state_dict()["bias"]})
 
 
 
